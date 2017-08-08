f467a73035c761afe721e8f534fd0e5ffefb9d90
Dragan Glumac <dragan.glumac@gmail.com>
2017-08-08 12:07:01 +0100

    The concept of unfold is majestic.

---
+++ 06-higher-order-functions/hofs.hs
@@ -0,0 +1,20 @@
+module HOFs where
+
+-- Excercise 11 - unfold
+
+unfold :: (b -> Bool) -> (b -> a) -> (b -> b) -> b -> [a]
+unfold p h t x
+  | p x = []
+  | otherwise = h x : unfold p h t (t x)
+
+type Bit = Int
+
+int2bin :: Int -> [Bit]
+--int2bin 0 = []
+--int2bin n = n `mod` 2 : int2bin (n `div` 2)
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+chop8 :: [Bit] -> [[Bit]]
+-- chop8 [] = []
+-- chop8 bits = take 8 bits : chop8 (drop 8 bits)
+chop8 = unfold null (take 8) (drop 8)
