50bb38ff77f9b8d7f9bd3591d69205174ce75a75
Dragan Glumac <dragan.glumac@gmail.com>
2017-08-08 12:19:09 +0100

    More unfold magic.

--- 06-higher-order-functions/hofs.hs
+++ 06-higher-order-functions/hofs.hs
@@ -1,8 +1,11 @@
 module HOFs where
 
 -- Excercise 11 - unfold
-
 unfold :: (b -> Bool) -> (b -> a) -> (b -> b) -> b -> [a]
+-- p :: (b -> Bool) - termination predicate
+-- h :: (b -> a)    - mapping from b to a
+-- t :: (b -> b)    - generation of next seed
+-- x :: b           - initial seed
 unfold p h t x
   | p x = []
   | otherwise = h x : unfold p h t (t x)
@@ -18,3 +21,13 @@ chop8 :: [Bit] -> [[Bit]]
 -- chop8 [] = []
 -- chop8 bits = take 8 bits : chop8 (drop 8 bits)
 chop8 = unfold null (take 8) (drop 8)
+
+
+-- Excerise 12
+unfoldMap :: (a -> b) -> [a] -> [b]
+unfoldMap f = unfold null (f . head) tail
+
+
+-- Excercise 13
+unfoldIterate :: (a -> a) -> a -> [a]
+unfoldIterate f = unfold (const False) id f
